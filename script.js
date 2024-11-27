c7c32c3423f0f2c0203420c783c0447d60b9058b
Sammie <osenisamad2004@gmail.com>
2024-11-27 09:16:52 -0800

    Removed the Mass Email Sender API

--- script.js
+++ script.js
@@ -59,11 +59,3 @@ const downElements = document.querySelectorAll("#down, #left, #right, .box-cells
 downElements.forEach(section => {
   downObserver.observe(section);
 });
-window.omnisend = window.omnisend || [];
-        omnisend.push(["accountID", "67474b3ae04e7c727b1ed747"]);
-        omnisend.push(["track", "$pageViewed"]);
-        !function(){var e=document.createElement("script");
-        e.type="text/javascript",e.async=!0,
-        e.src="https://omnisnippet1.com/inshop/launcher-v2.js";
-        var t=document.getElementsByTagName("script")[0];
-        t.parentNode.insertBefore(e,t)}();
